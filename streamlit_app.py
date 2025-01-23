df60d24c5ede9e2ae24a49c3e8d5a6042c15e141
dksudheer612 <ksdamarla612@gmail.com>
2025-01-23 11:49:55 -0600

    Update streamlit_app.py
    committing requests python package

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,25 +9,14 @@ st.write(
     """**Choose the fruits you want in your custom Smoothie..!**"""
 )
 
-#option = st.selectbox(
- #   "What is your favorite Fruit?",
-  #  ('Banana', 'Strawberries', 'Peaches'),
-#)
-
-#st.write("You selected:", option)
-
 import streamlit as st
 name_on_order = st.text_input('Name on Smoothie:')
 st.write ('The Name on your Smoothie will be: ', name_on_order)
 
-
-
 cnx = st.connection("snowflake")
 session = cnx.session()
 #session = get_active_session()
-
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list = st.multiselect('Choose up to 5 ingredients: '
@@ -54,3 +43,7 @@ if time_to_insert:
        session.sql(my_insert_stmt).collect()
            
        st.success('Your Smoothie is ordered!', icon="✅")
+
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
